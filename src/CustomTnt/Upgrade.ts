83efa984f061218ec0a8931e5ae28916d52d86e4
Mizab <mizabmansari@gmail.com>
2024-03-23 23:53:36 +0530

    Added upgrade demo

---
+++ src/CustomTnt/Upgrade.ts
@@ -0,0 +1,6 @@
+import { MCFunction } from "sandstone";
+import { upgradeTNTGenerics } from "./Private/ComputerHandler/InteractionHandler";
+
+export const upgradeTNT = MCFunction("custom_tnt/upgrade_tnt", () => {
+  upgradeTNTGenerics("5x.stable", "5x.risky", 110002);
+});
