ceccaece9f151f925ee54e91053cc9f7956b4b65
Mizab <mizabmansari@gmail.com>
2024-03-23 23:53:50 +0530

    Changed the name to lower case

--- src/CustomTnt/private/AddGravityToTnt.ts
+++ src/CustomTnt/private/AddGravityToTnt.ts
@@ -1,7 +1,7 @@
 import { MCFunction, NBT, Selector, data, execute } from "sandstone";
 import { self } from "../../Tick";
 
-export const AddGravity = MCFunction("custom_tnt/auxillary/gravity_tnt/add_gravity", () => {
+export const addGravity = MCFunction("custom_tnt/auxillary/gravity_tnt/add_gravity", () => {
   // Add the gravity to the TNT
   execute
     .as(
